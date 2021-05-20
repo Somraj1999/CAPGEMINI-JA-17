efa4fc11a3d671423e0af3ada95af9eb17c6049b
Somraj1999 <84119973+Somraj1999@users.noreply.github.com>
2021-05-20 10:48:52 +0530

    Create mirror_string.java

---
+++ LAB BOOK/Lab 3/mirror_string.java
@@ -0,0 +1,14 @@
+public class Mirror_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "SOMRAJ";
+        for(int i=s1.length()-1;i>=0;i--)
+            {
+                System.out.print(s1.charAt(i));
+            }
+
+	}
+
+}
