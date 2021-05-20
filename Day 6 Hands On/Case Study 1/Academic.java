4c132ffc4a1e7166dfc8864605e66add8aeb670b
Somraj1999 <84119973+Somraj1999@users.noreply.github.com>
2021-05-20 10:57:42 +0530

    Create Academic.java

---
+++ Day 6 Hands On/Case Study 1/Academic.java
@@ -0,0 +1,21 @@
+package case_study_1;
+
+public class Academic extends Payroll {
+
+	
+	private String name;
+    private int salary;
+
+    Academic() {}
+
+    public Academic(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+        super.adjustSalary(salary);
+    }
+
+    public void giveLecture() {
+        System.out.println("Giving lecture");
+}
+    
+}
